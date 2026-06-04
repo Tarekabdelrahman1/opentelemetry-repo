0c6d6df1c3f06e56436739a7fe92c0617819ce84
Tarekabdelrahman1 <tarekabdelrahman2002@gmail.com>
2026-06-04 16:49:49 +0300

    Update gitignore and add project files

---
+++ EKS-create/backend/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "demo-terraform-eks-state-s3-tarek-bucket"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-eks-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
